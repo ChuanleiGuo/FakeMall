aff19d4c6be83e19980616bffc85a9f46fe1d7fe
ChuanleiGuo <chuanleiguo@gmail.com>
2018-06-23 16:16:31 +0800

    added implemented user register and get password

--- src/main/java/com/fakemall/controller/api/UserController.java
+++ src/main/java/com/fakemall/controller/api/UserController.java
@@ -19,6 +19,13 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    /**
+     * login
+     * @param username
+     * @param password
+     * @param session
+     * @return
+     */
     @RequestMapping(value = "login", method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse<User> login(String username, String password, HttpSession session) {
@@ -27,5 +34,37 @@ public class UserController {
             session.setAttribute(Const.CURRENT_USER, response.getData());
         }
         return response;
+    }
+
+    @RequestMapping(value = "logout", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<String> logout(HttpSession session) {
+        session.removeAttribute(Const.CURRENT_USER);
+        return ServerResponse.createBySuccess();
+    }
+
+    @RequestMapping(value = "register", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> register(User user) {
+        return userService.register(user);
+    }
+
+    @RequestMapping(value = "check_valid", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> checkValid(String str, String type) {
+        return userService.checkValid(str, type);
+    }
+
+    @RequestMapping(value = "forget_get_question",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> forgetGetQuestion(String username){
+        return userService.selectQuestion(username);
+    }
+
+
+    @RequestMapping(value = "forget_check_answer",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> forgetCheckAnswer(String username,String question,String answer){
+        return userService.checkAnswer(username,question,answer);
     }
 }
--- src/main/java/com/fakemall/service/impl/UserServiceImpl.java
+++ src/main/java/com/fakemall/service/impl/UserServiceImpl.java
@@ -1,12 +1,17 @@
 package com.fakemall.service.impl;
 
 import com.fakemall.common.ServerResponse;
+import com.fakemall.common.TokenCache;
 import com.fakemall.dao.UserMapper;
 import com.fakemall.model.User;
 import com.fakemall.service.UserService;
+import com.fakemall.common.Const;
+import com.fakemall.util.MD5Util;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service("userService")
 public class UserServiceImpl implements UserService {
@@ -21,13 +26,84 @@ public class UserServiceImpl implements UserService {
             return ServerResponse.createByErrorMessage("user does not exist");
         }
 
-        //TODO: MD5
-        User user = userMapper.selectLogin(username, password);
+        String md5Password = MD5Util.MD5EncodeUtf8(password);
+        User user = userMapper.selectLogin(username, md5Password);
         if (user == null) {
             return ServerResponse.createByErrorMessage("wrong password");
         }
 
         user.setPassword(StringUtils.EMPTY);
         return ServerResponse.createBySuccess("success", user);
+    }
+
+    @Override
+    public ServerResponse<String> register(User user) {
+        ServerResponse<String> validResponse = checkValid(user.getUsername(), Const.USERNAME);
+        if (!validResponse.isSuccess()) {
+            return validResponse;
+        }
+
+        validResponse = checkValid(user.getEmail(), Const.EMAIL);
+        if (!validResponse.isSuccess()) {
+            return validResponse;
+        }
+
+        user.setRole(Const.Role.ROLE_CUSTOMER);
+        user.setPassword(MD5Util.MD5EncodeUtf8(user.getPassword()));
+
+        int resultCount = userMapper.insert(user);
+        if (resultCount == 0) {
+            return ServerResponse.createByErrorMessage("register fail");
+        }
+        return ServerResponse.createBySuccessMessage("register success");
+    }
+
+    @Override
+    public ServerResponse<String> checkValid(String str, String type) {
+        if (StringUtils.isBlank(type)) {
+            return ServerResponse.createByErrorMessage("wrong argument");
+        }
+
+        switch (type) {
+            case Const.EMAIL:
+                if (userMapper.checkEmail(str) > 0) {
+                    return ServerResponse.createByErrorMessage("email exists");
+                }
+                break;
+            case Const.USERNAME:
+                if (userMapper.checkUsername(str) > 0) {
+                    return ServerResponse.createByErrorMessage("user exists");
+                }
+                break;
+            default:
+                return ServerResponse.createByErrorMessage("unsupported check type");
+        }
+
+        return ServerResponse.createBySuccessMessage("check success");
+    }
+
+    @Override
+    public ServerResponse<String> selectQuestion(String username) {
+        ServerResponse<String> validResponse = checkValid(username, Const.USERNAME);
+        if (validResponse.isSuccess()) {
+            return ServerResponse.createByErrorMessage("user does not exist");
+        }
+        String question = userMapper.selectQestionByUsername(username);
+        if (StringUtils.isNotBlank(question)) {
+            return ServerResponse.createBySuccess(question);
+        }
+        return ServerResponse.createByErrorMessage("question is empty");
+    }
+
+    @Override
+    public ServerResponse<String> checkAnswer(String username, String question, String answer) {
+        int resultCount = userMapper.checkAnswer(username,question,answer);
+        if(resultCount > 0){
+            //说明问题及问题答案是这个用户的,并且是正确的
+            String forgetToken = UUID.randomUUID().toString();
+            TokenCache.setKey(TokenCache.TOKEN_PREFIX+username,forgetToken);
+            return ServerResponse.createBySuccess(forgetToken);
+        }
+        return ServerResponse.createByErrorMessage("问题的答案错误");
     }
 }
